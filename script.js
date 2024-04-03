529f91400527fe12275592744a8f48f04e683517
Abubakr <abubakrdev69@gmail.com>
2024-04-03 11:13:50 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+let count = 0;
+const countDisplay = document.getElementById("count");
+
+function increment() {
+    count++;
+    countDisplay.textContent = count;
+}
+
+function decrement() {
+    if(count != 0) count--;
+    countDisplay.textContent = count;
+}
+
+function reloadPage() {
+    location.reload();
+}
